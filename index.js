936c1bd724e75bc7b28f60f6addb6240173ea271
Sebastian Price <skphoopa@yahoo.com>
2018-06-18 13:50:54 -0400

    added links to elements

--- index.js
+++ index.js
@@ -17,7 +17,10 @@ app.get('/', (req, res) => {
 
 //contacts list page show the user all contacts
 app.get('/contacts', (req, res) => {
-   res.send(contacts.users);
+ //  res.send(contacts.users);
+ res.render('contacts-list', { 
+    contactsArray: contacts.users
+ });
 });
 
 //contacts details page show the user all info for one contact
