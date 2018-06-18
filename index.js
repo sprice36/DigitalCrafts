afdf65eddb46b8e116d3ae62a4f0192b19c14c24
Sebastian Price <skphoopa@yahoo.com>
2018-06-18 16:04:44 -0400

    image scraper exercise

--- index.js
+++ index.js
@@ -1,25 +1,66 @@
-const rp = require('request-promise');
+const makeRequest = require('request-promise');
+const request = require('request');
 const cheerio = require('cheerio');
+const fs = require('fs');
 
-const URL = `https://news.ycombinator.com/`;
+const URL = 'https://www.reddit.com/';
 
-const options = {
-  uri: URL,
-  transform: (body) => {
-    return cheerio.load(body);
-  }
-};
+function processDataFromServer(respData) {
+  // console.log(respData);
+  // I could dump all this to a file.
+  let $ = cheerio.load(respData);
 
-rp(options)
-  .then(($) => {
-    // the code will look very much
-    // like jQuery code
-    $('.storylink').each( (i, element) => {
-      // console.log(element);
-      // re-cheerio the thing!
-      console.log( $(element).text() );
-    });
-  })
-  .catch((err) => {
-    console.log(err);
+  $('img').each((i, element) => {
+    var imageURL = $(element).attr('src');
+    if (imageURL) {
+      if (!imageURL.startsWith('https')) {
+        imageURL = 'https:' + imageURL;
+      }
+      // #1 download the image
+      console.log(`requesting...${imageURL}`);
+
+      request(imageURL, {encoding: 'binary'}, saveImage);
+        // .then(downloadImage)
+        // .catch(thatErrorTho)
+    }
   });
+}
+
+function saveImage(error, response, imageData) {
+  if (error) {
+    thatErrorTho(error);
+    return;
+  }
+
+  // console.log(imageData);
+  console.log('downloading...image...');
+  let imageName = 'downloadedImages/' + (new Date()).getTime() + '.jpg';
+
+  fs.writeFile(imageName, imageData, 'binary', thatErrorTho);
+}
+
+function thatErrorTho(err) {
+  console.log(err);
+}
+
+makeRequest(URL)
+  .then(processDataFromServer)
+  .catch(thatErrorTho)
+
+
+
+
+
+// let someHTML = `
+//   <h1>
+//     hey what is up?
+//   </h1>
+//   <img src="oakley.jpg">
+// `;
+
+// let $ = cheerio.load(someHTML);
+
+// let theText = $('img').attr('src');
+// console.log(theText);
+
+
