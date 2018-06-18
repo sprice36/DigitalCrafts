8ee1c015822962b427f0d7aecd5f42e2905de0e5
Sebastian Price <skphoopa@yahoo.com>
2018-06-18 11:19:58 -0400

    adds routes for contacts and contacts/id

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const app = express();
 
+//show the user a welcome message 
 app.get('/', (req, res) => {
-    res.send('hey');
+    res.send('hey its the home page');
 });
+
+//contacts list page show the user all contacts
+app.get('/contacts', (req, res) => {
+   res.send('on the listing page');
+});
+
+//contacts details page show the user all info for one contact
+app.get('/contacts/:id', (req,res) => {
+    res.send(`you are viewing details for ${req.params.id}`);
+});
+
 
 app.listen(8888, () => {
     console.log('Your express app is running at http://localhost:8888');
