f9fdb03e65e0973318d70e21c35ae0fd5358b1ae
Sebastian Price <skphoopa@yahoo.com>
2018-06-22 14:56:52 -0400

    adding index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+const Todo = require('./db');
+
+app.get('/', (req, res) => {
+    Todo.getAll()
+      .then((data) => {
+          console.log(data);
+          res.send(data);
+      })
+      .catch((error) => {console.log(error); });
+});
+
+app.listen(4000);
