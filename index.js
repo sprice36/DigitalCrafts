7028f634ecf8d13d76634c49a16cd9b87eb446e4
Sebastian Price <skphoopa@yahoo.com>
2018-06-22 15:04:53 -0400

    updated index.js

--- index.js
+++ index.js
@@ -6,10 +6,20 @@ const Todo = require('./db');
 app.get('/', (req, res) => {
     Todo.getAll()
       .then((data) => {
-          console.log(data);
+        //  console.log(data);
           res.send(data);
       })
       .catch((error) => {console.log(error); });
+});
+
+app.get('/:id', (req,res) =>{
+     Todo.getOne(req.params.id)
+     .then((data) => {
+        console.log(data);
+        res.send(data);
+        //res.render('todo-detail-page' , data);
+    })
+    .catch((error) => {console.log(error); });
 });
 
 app.listen(4000);
